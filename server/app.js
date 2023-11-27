69786393699020d2f87244ca09857c1532a61d75
fauzandwip <fauzan27dp@gmail.com>
2023-11-27 22:02:00 +0700

    feat: connect server to mongodb atlas

--- server/app.js
+++ server/app.js
@@ -1,10 +1,21 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
 const { userTypeDefs, userResolvers } = require('./schemas/user');
+const { MongoClient } = require('mongodb');
+
+const uri =
+	'mongodb+srv://fauzandp:<password>@cluster0.bg37y6c.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri);
 
 const server = new ApolloServer({
 	typeDefs: [userTypeDefs],
 	resolvers: [userResolvers],
+	context: async () => {
+		if (!client.connect()) client.connect();
+		return {
+			client: client.db('FacebookApp_p3'),
+		};
+	},
 });
 
 startStandaloneServer(server, {
