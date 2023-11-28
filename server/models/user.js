33097b354f90a3ce190da7913be67fec61d71dec
fauzandwip <fauzan27dp@gmail.com>
2023-11-28 19:14:54 +0700

    refactor: move all validation from model to resolver

--- server/models/user.js
+++ server/models/user.js
@@ -5,61 +5,25 @@ const { comparePassword, hashPassword } = require('../helpers/bcrypt');
 const { signToken } = require('../helpers/jwt');
 
 class User {
-	static async register({ name, username, email, password }) {
+	static async addUser(user) {
 		const users = getDB().collection('users');
-		const user = await users.findOne({
-			$or: [{ email }, { username }],
-		});
-
-		if (user) {
-			if (user.email === email) {
-				throw new GraphQLError('Email already exists', {
-					extensions: { code: 'BAD_USER_INPUT' },
-				});
-			}
-
-			if (user.username === username) {
-				throw new GraphQLError('Username must be unique', {
-					extensions: { code: 'BAD_USER_INPUT' },
-				});
-			}
-		}
-
-		const response = await users.insertOne({
-			name,
-			username,
-			email,
-			password: hashPassword(password),
-		});
-
+		const response = await users.insertOne(user);
 		const newUser = await users.findOne({ _id: response.insertedId });
 		return newUser;
 	}
 
-	static async login({ email, password }) {
-		const user = await getDB().collection('users').findOne({ email });
-
-		if (!user || !comparePassword(password, user.password)) {
-			throw new GraphQLError('Invalid email/password', {
-				extensions: { code: 'BAD_USER_INPUT' },
+	static async getUserByEmailOrUsername(email, username) {
+		return await getDB()
+			.collection('users')
+			.findOne({
+				$or: [{ email }, { username }],
 			});
-		}
-
-		return {
-			access_token: signToken({
-				id: user._id,
-				email: user.email,
-			}),
-		};
 	}
 
 	static async getUserById(id) {
-		if (!id) {
-			throw new GraphQLError('User not found');
-		}
-		const users = getDB().collection('users');
-		const user = await users.findOne({ _id: new ObjectId(id) });
-		return user;
+		return await getDB()
+			.collection('users')
+			.findOne({ _id: new ObjectId(id) });
 	}
 }
 
--- server/schemas/user.js
+++ server/schemas/user.js
@@ -1,4 +1,7 @@
+const { GraphQLError } = require('graphql');
+const { hashPassword, comparePassword } = require('../helpers/bcrypt');
 const User = require('../models/user');
+const { signToken } = require('../helpers/jwt');
 const usersData = [
 	{
 		_id: 1,
@@ -46,7 +49,18 @@ const userResolvers = {
 		user: async (_, args) => {
 			try {
 				const { id } = args;
+				if (!id) {
+					throw new GraphQLError('User not found', {
+						extensions: { code: 'NOT_FOUND' },
+					});
+				}
+
 				const user = await User.getUserById(id);
+				if (!user) {
+					throw new GraphQLError('User not found', {
+						extensions: { code: 'NOT_FOUND' },
+					});
+				}
 
 				return user;
 			} catch (error) {
@@ -58,11 +72,58 @@ const userResolvers = {
 		register: async (_, args) => {
 			try {
 				const { name, username, email, password } = args.user;
-				const newUser = await User.register({
+
+				if (!username) {
+					throw new GraphQLError('Username is required', {
+						extensions: { code: 'BAD_USER_INPUT' },
+					});
+				}
+
+				if (!email) {
+					throw new GraphQLError('Email is required', {
+						extensions: { code: 'BAD_USER_INPUT' },
+					});
+				}
+
+				if (!password) {
+					throw new GraphQLError('Password is required', {
+						extensions: { code: 'BAD_USER_INPUT' },
+					});
+				}
+
+				if (!email.includes('@')) {
+					throw new GraphQLError('Invalid email format', {
+						extensions: { code: 'BAD_USER_INPUT' },
+					});
+				}
+
+				if (password.length < 5) {
+					throw new GraphQLError('Password must be more than 5 characters', {
+						extensions: { code: 'BAD_USER_INPUT' },
+					});
+				}
+
+				const user = await User.getUserByEmailOrUsername(email, username);
+
+				if (user) {
+					if (user.email === email) {
+						throw new GraphQLError('Email already exists', {
+							extensions: { code: 'BAD_USER_INPUT' },
+						});
+					}
+
+					if (user.username === username) {
+						throw new GraphQLError('Username must be unique', {
+							extensions: { code: 'BAD_USER_INPUT' },
+						});
+					}
+				}
+
+				const newUser = await User.addUser({
 					name,
 					username,
 					email,
-					password,
+					password: hashPassword(password),
 				});
 
 				return newUser;
@@ -73,12 +134,20 @@ const userResolvers = {
 		login: async (_, args) => {
 			try {
 				const { email, password } = args;
-				const token = await User.login({
-					email,
-					password,
-				});
+				const user = await User.getUserByEmailOrUsername(email);
 
-				return token;
+				if (!user || !comparePassword(password, user.password)) {
+					throw new GraphQLError('Invalid email/password', {
+						extensions: { code: 'BAD_USER_INPUT' },
+					});
+				}
+
+				return {
+					access_token: signToken({
+						id: user._id,
+						email: user.email,
+					}),
+				};
 			} catch (error) {
 				throw error;
 			}
